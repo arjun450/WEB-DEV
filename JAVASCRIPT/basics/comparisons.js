b30f797bcdda83a53fd33abb1d26c398ced17838
arjun450 <arjunvishnoi30@gmail.com>
2026-06-14 18:21:05 +0530

    Update comparisons.js

---
+++ JAVASCRIPT/basics/comparisons.js
@@ -0,0 +1,37 @@
+// console.log(2>1) //true
+// console.log(2<1) //false
+// console.log(2>=1) //true
+// console.log(2<=1) //false
+// console.log(2==1) //false
+// console.log(2!=1) //true
+// console.log(2===1) //false
+// console.log(2!==1) //true
+
+// == and comparison operators(>=,>,<) mei farak hota h 
+//=== data type aur value dono check krta h
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
+
+
+
+
+
+
+
+
+
+
+
